7401cd66fc3a9d5bfb5956d755d9fd21c3d4cab0
altaris33 <jeremie.laera@live.fr>
2026-05-01 13:50:45 +0200

    Initial commit: added Spring Data JPA and mariadb dependencies and created a Spring Boot Command Line App wih DB configurations

---
+++ src/main/resources/01-create-user.sql
@@ -0,0 +1,9 @@
+-- Drop user first if they exist
+DROP USER if exists 'springstudent'@'%' ;
+DROP USER if exists 'springstudent'@'localhost' ;
+
+-- Now create user with prop privileges
+--CREATE USER 'springstudent'@'%' IDENTIFIED BY 'springstudent';
+CREATE USER 'springstudent'@'localhost' IDENTIFIED BY 'springstudent';
+
+GRANT ALL PRIVILEGES ON * . * TO 'springstudent'@'localhost';
